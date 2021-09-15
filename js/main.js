0426b99c22042f86de1c5354935ff820b1c5d4c9
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2021-09-15 18:10:27 +0200

    Updated About section

--- js/main.js
+++ js/main.js
@@ -43,3 +43,20 @@ $(document).ready(function() {
     //AOS instance
     AOS.init();
 });
+
+(function ($) {
+    "use strict";
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+})(jQuery);
+
